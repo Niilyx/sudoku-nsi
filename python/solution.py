035c06678a5d9512b94a034d122679bbf1f2b049
AXXIAR <galoualexis@gmail.com>
2022-03-27 17:31:25 +0200

    Pas loin du but ?

--- python/solution.py
+++ python/solution.py
@@ -11,11 +11,10 @@ Notes :
             chaque Cell a donc une valeur, ainsi qu'un x et un y correspondant à sa place dans le tableau 
 """
 from math import sqrt
-from random import choice, randint
-from time import sleep
+from random import choice
 
 class Cell:
-    """doc needed"""
+    """Classe Cell, représente chaque cellule (case) du sudoku"""
     def __init__(self,value,x,y):
         """Constructeur
         Arguments :
@@ -60,102 +59,106 @@ class Cell:
             elif 6 <= self.x <= 8 and 6 <= self.y <= 8:
                 return 9
 
+
 class Sudoku:
-    """doc needed"""
-    def __init__(self,size=4):  # je met la taille du sudoku à 4x4 par défaut pour l'instant
+    """Classe Sudoku, réprésente le sudoku entier"""
+    def __init__(self,size=4):
         """Constructeur
         Arguments :
             - size : taille du sudoku (4x4 ou 9x9), par défaut 4
+            - grid : listes imbriquées représentant la grille de sudoku
         """
         self.size = size
-        # self.tableau = [
-        #     [Cell(2,0,0),Cell(3,1,0),Cell(1,2,0),Cell(4,3,0)],
-        #     [Cell(4,0,1),Cell(1,1,1),Cell(3,2,1),Cell(2,3,1)],
-        #     [Cell(1,0,2),Cell(2,1,2),Cell(3,2,2),Cell(3,3,2)],
-        #     [Cell(3,0,3),Cell(4,1,3),Cell(2,2,3),Cell(1,3,3)],
-        # ]
-        self.tableau = self.__generate_empty_sudoku()  # tableau qui représente le sudoku
+        self.grid = self.__generate_empty_sudoku()  # la grille est vide au départ
         
 
     # --- Fonctions modifiants le sudoku ---
+
     def __generate_empty_sudoku(self):
         """int -> list[int]
-        Génére et renvoie un sudoku vide en fonction de la taille précisée ('size')
+        Génére et renvoie une liste d'objet 'Cell' (dont la valeur est 0) en fonction de 'self.size'
         """
         return [[Cell(0,i,int(j/self.size)) for i in range(self.size)] for j in range(0,self.size**2,self.size)]
         
 
-    def generate_sudoku(self):      # TRES basique pour l'instant et pas opti DU TOUT (no way)
-
+    def generate_sudoku(self):      # assez basique, à améliorer si possible
+        """Modifie 'self.grid' en générant un sudoku entier dont la taille dépend de 'self.size'"""
+        
+        # while not self.is_complete():
         while True:
-            random_cell = self.find_empty_cell()
-            if not random_cell:
+            empty_cell = self.find_empty_cell()
+            if not empty_cell:
                 break
-            print(random_cell.x,random_cell.y)
             
-            random_list = [i for i in range(1,self.size+1)]
-            new_cell = choice(random_list)
+            numbers = [i for i in range(1,self.size+1)] # Les possibilités à tester
+            new_value = choice(numbers)
 
             while True:
 
-                move_is_correct = self.is_correct(random_cell,new_cell)
+                move_is_correct = self.is_correct(empty_cell,new_value)
                 
                 if move_is_correct == True:
-                    print("je pose")
-                    random_cell.value = new_cell
+                    # on pose le chiffre
+                    empty_cell.value = new_value
                     break
 
                 else :
                     try:
-                        print("je change de nombre")
-                        random_list.remove(new_cell)
-                        new_cell = choice(random_list)
+                        # on réessaye avec un autre chiffre
+                        numbers.remove(new_value)
+                        new_value = choice(numbers)
                     except:
-                        print("je swap")
+                        # on échange les cases
                         old_cell = move_is_correct[0][move_is_correct[1]]
                         old_cell.value = 0
-                        random_cell.value = new_cell
+                        empty_cell.value = new_value
                         break
-                print("---")
             self.debug()
+            print("---")
 
+
+    # --- Fonctions récupérants des données sur le sudoku (getters,...) ---
 
     def find_empty_cell(self):
-        for i in self.tableau:
+        """-> Cell | None
+        Renvoie la première case de la grille dont la valeur est 0, renvoie None s'il n'y en a pas"""
+        for i in self.grid:
             for j in i:
                 if j.value == 0:
                     return j
         return None
 
-    # --- Fonctions récupérants des données sur le sudoku (getters,...) ---
     def get_column(self,column,values=True):
-        """int * bool -> lis[int] | False
-        Renvoie une liste contenant les numéros de la colonne donnée en paramètres ('column')"""
+        """int * bool -> list[int] | list[Cell] | False
+        Renvoie une liste contenant les numéros de la colonne donnée en paramètres ('column') si 'values' == True
+                                                                                              sinon renvoies la liste même liste mais avec des objet Cell directement"""
         if column > self.size or column < 1:
             return False
 
         if values:
-            return [self.tableau[i][column-1].value for i in range(len(self.tableau))]
+            return [self.grid[i][column-1].value for i in range(len(self.grid))]
         else:
-            return [self.tableau[i][column-1] for i in range(len(self.tableau))]
+            return [self.grid[i][column-1] for i in range(len(self.grid))]
 
 
     def get_row(self,row,values=True):
-        """int * bool -> list[int] | False
-        Renvoie une liste contenant les numéros de la ligne donnée en paramètres ('row')"""
+        """int * bool -> list[int] | list[Cell] | False
+        Renvoie une liste contenant les numéros de la ligne donnée en paramètres ('row') si 'values' == True
+                                                                                           sinon renvoies la liste même liste mais avec des objet Cell directement"""
         if row > self.size or row < 1:
             return False
 
         if values:
-            return [i.value for i in self.tableau[row-1]]
+            return [i.value for i in self.grid[row-1]]
         else:
-            return [i for i in self.tableau[row-1]]
+            return [i for i in self.grid[row-1]]
 
 
 
     def get_square(self,square,values=True):    # pas du tout opti, à amélorer si possible
-        """int * bool -> list[int] | False
-        Renvoie une liste contenant les numéros du carré donné en paramètres ('square')"""
+        """int * bool -> list[int] | list[Cell] | False
+        Renvoie une liste contenant les numéros du carré (sous-grille) donné en paramètres ('square') si 'values' == True
+                                                                                                      sinon renvoies la liste même liste mais avec des objet Cell directement"""
         if square < 1 or square > self.size:
             return False
 
@@ -165,13 +168,13 @@ class Sudoku:
         y_min = ((square-1)//sr_size)*sr_size
 
         if values:
-            for j in self.tableau:
+            for j in self.grid:
                 for k in j:
                     if x_min <= k.x < x_min+sr_size and y_min <= k.y < y_min*sr_size+sr_size:
                         l.append(k.value)
         
         else:
-            for j in self.tableau:
+            for j in self.grid:
                 for k in j:
                     if x_min <= k.x < x_min+sr_size and y_min <= k.y < y_min*sr_size+sr_size:
                         l.append(k)
@@ -179,37 +182,41 @@ class Sudoku:
     
 
     # --- Fonctions de tests et aide ---
+
     def is_complete(self):
         """renvoie True si le sudoku est entier sinon False"""
         for i in range(self.size):
             for j in range(self.size):
-                current_cell = self.tableau[i][j]
+                current_cell = self.grid[i][j]
                 if current_cell.value == 0 or self.get_row(i+1).count(current_cell.value) > 1 or self.get_column(j+1).count(current_cell.value) > 1 or self.get_square(current_cell.get_square(self.size)).count(current_cell.value) > 1:
                     return False
         return True
 
-    def is_correct(self,cell,new_cell):
-        """Cell*int-> bool
-        Renvoie True si 'new_cell' peut être placée aux coordonées x,y de 'cell'
-            sinon renvoie False"""
-        x,y,square = cell.x,cell.y,cell.get_square(self.size)     # square est le numéro carré auqel appartient la case
-        
-        if new_cell in self.get_row(y+1):
-            return self.get_row(y+1,False),self.get_row(y+1).index(new_cell)
 
-        elif new_cell in self.get_column(x+1):
-            return self.get_column(x+1,False),self.get_column(x+1).index(new_cell)
+    def is_correct(self,cell,new_value):
+        """Cell*int -> tuple(list[Cell],int) | True
+        Renvoie True si 'new_value' peut être placée aux coordonées x,y de 'cell'
+            sinon renvoie un tuple de la liste des cellules où se trouve l'élément dupliqué, ainsi que de l'index de 'new_value' dans cette même liste ([Cell],int)"""
+        x,y,square = cell.x,cell.y,cell.get_square(self.size)
 
-        elif new_cell in self.get_square(square):
-            return self.get_square(square,False),self.get_square(square).index(new_cell)
+        # si la ligne contient la nouvelle valeur
+        if new_value in self.get_row(y+1):  
+            return self.get_row(y+1,False),self.get_row(y+1).index(cell.value)
+
+        # si la colonne contient la nouvelle valeur
+        elif new_value in self.get_column(x+1): 
+            return self.get_column(x+1,False),self.get_column(x+1).index(cell.value)
+
+        # si le carré contient la nouvelle valeur
+        elif new_value in self.get_square(square):  
+            return self.get_square(square,False),self.get_square(square).index(cell.value)
 
         return True
 
 
     def debug(self):
-        """-> str
-        Print le sudoku actuel dans un format plus lisible"""        
-        for i in self.tableau:
+        """Print le sudoku actuel dans un format plus lisible"""        
+        for i in self.grid:
             print([i[j].value for j in range(len(i))])
 
 
@@ -218,14 +225,8 @@ class Sudoku:
 
 if __name__ == "__main__":
     sk_4x4 = Sudoku() # 4x4 par défaut
-    sk_9x9 = Sudoku(9)
+    sk_9x9 = Sudoku(4)
 
-    # # sk_4x4.debug()
-    # sk_9x9.tableau = [
-    #     [Cell(2,0,0),Cell(3,1,0),Cell(1,2,0),Cell(4,3,0)],
-    #     [Cell(4,0,1),Cell(1,1,1),Cell(3,2,1),Cell(2,3,1)],
-    #     [Cell(1,0,2),Cell(2,1,2),Cell(4,2,2),Cell(3,3,2)],
-    #     [Cell(3,0,3),Cell(4,1,3),Cell(2,2,3),Cell(1,3,3)],
-    # ]
     sk_9x9.generate_sudoku()
+    sk_9x9.debug()
     print("Le sudoku est complet : ",sk_9x9.is_complete())
