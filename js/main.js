dcb00aa3cbcfaa9249d02f70bd5b8b639eed4c83
Niilyx _ <nait.meddour.idir100@gmail.com>
2022-04-02 18:34:16 +0200

    s

--- js/main.js
+++ js/main.js
@@ -7,10 +7,37 @@ var bgMusic = [new Audio("media/bg1.mp3"),
                new Audio("media/bg2.mp3"),];
 var currentMusic;
 
-window.onload = function () {
-    alert(window.location.search)
+var pseudo;
+var difficulte;
+var aide;
+
+window.onload = () => {
+    let passedArgs = new URLSearchParams(window.location.search)
+    pseudo = passedArgs.get("pseudo")
+    difficulte = passedArgs.get("difficulty")
+    aide = passedArgs.has("aide")
+    
+    // obligé de cleanup, au cas où un petit rigolo change l'URL...
+    if (pseudo == "") {
+        pseudo = "L'anonyme dissident"
+    }
+    
+    let isDifficValid = false
+    for (let i of Array.apply(null, document.getElementById("time").options)) {
+        if (difficulte == i.value) {
+            isDifficValid = true
+            break
+        }
+    }
+    if (!isDifficValid) document.getElementById("time").value = "15min"
+    else document.getElementById("time").value = difficulte
+
     setUp();
     brython();
+}
+
+function wereArgsPassed() {
+    return window.location.search != ""
 }
 
 function addClass(id, classe) {
